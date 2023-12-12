5f681e4cd717205237a80e362ce315dca936b2c7
viniciuscsc <viniciuscsc@icloud.com>
2023-12-12 15:14:57 -0300

    Req 02: cria a model Profile e a migration 0001_initial

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    github = models.URLField()
+    linkedin = models.URLField()
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name

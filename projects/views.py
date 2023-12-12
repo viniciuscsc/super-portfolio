b918e2f32d1b42a68cd6ea18a4044410d604c9f4
viniciuscsc <viniciuscsc@icloud.com>
2023-12-12 16:05:25 -0300

    Req 02: cria o ProfileView

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from projects.models import Profile
+from projects.serializers.profile_serializer import ProfileSerializer
+
 
 # Create your views here.
+class ProfileView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer

00f4008930009d48f3bbd1040983f883f74ea8f3
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 16:21:35 -0300

    Req 05: registra as models em projects/admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from projects.models import (
+    Profile,
+    Project,
+    Certificate,
+    CertifyingInstitution,
+)
+
 
 # Register your models here.
+class CertificateInline(admin.StackedInline):
+    model = Certificate
+
+
+class CertifyingInstitutionAdmin(admin.ModelAdmin):
+    inlines = [CertificateInline]
+
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(
+    CertifyingInstitution,
+    CertifyingInstitutionAdmin,
+)

40c1b349017ebaad1609d3c068a31ede1b4d2034
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 12:20:17 -0300

    Req 03: atualiza importações em projects/views.py

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from projects.models import Profile
-from projects.serializers.profile_serializer import ProfileSerializer
-from projects.serializers.project_serializer import ProjectSerializer
+from projects.serializers import ProfileSerializer, ProjectSerializer
 
 
 # Create your views here.

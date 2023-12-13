cf4872ad5e8fb852e57bafd84ce48d714ce2ea94
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 12:01:25 -0300

    Req 03: cria a rota projects/

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
-from projects.views import ProfileView
+from projects.views import ProfileView, ProjectView
 
 router = routers.DefaultRouter()
 router.register("profiles", ProfileView)
+router.register("projects", ProjectView)
 
 urlpatterns = [path("", include(router.urls))]

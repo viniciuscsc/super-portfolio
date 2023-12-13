9d8190f8abc95ae3c7d92730f4bcd4688264dcfd
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 14:47:47 -0300

    Req 04: implementa get_permissions() e retrieve() em ProfileView

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from projects.models import Profile, Project
 from projects.serializers import ProfileSerializer, ProjectSerializer
 
@@ -8,6 +10,20 @@ from projects.serializers import ProfileSerializer, ProjectSerializer
 class ProfileView(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            context = {"profile": self.get_object()}
+
+            return render(request, "profile.html", context)
+
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectView(viewsets.ModelViewSet):

e584977c9a66e073686803375e08ca711416cf3e
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 16:05:00 -0300

    Req 05: cria rotas certifying-institutions/ e certificates/

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
-from projects.views import ProfileViewSet, ProjectView
+from projects.views import (
+    ProfileViewSet,
+    ProjectView,
+    CertificateView,
+    CertifyingInstitutionView,
+)
 
 router = routers.DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
 router.register(r"projects", ProjectView)
+router.register(r"certifying-institutions", CertifyingInstitutionView)
+router.register(r"certificates", CertificateView)
 
 urlpatterns = [path("", include(router.urls))]

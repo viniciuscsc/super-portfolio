3c1ee6e545cac261cd7dbbd771cb5f962b08a90d
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 11:52:45 -0300

    Req 03: cria model Project e migration 0002_project

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,19 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=500, blank=False)
+    github_url = models.URLField(blank=False)
+    keyword = models.CharField(max_length=50, blank=False)
+    key_skill = models.CharField(max_length=50, blank=False)
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="projects",
+    )
+
+    def __str__(self) -> str:
+        return self.name

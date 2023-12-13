357a570c369bd544c8302c69f955b6bf2d3e35ba
viniciuscsc <viniciuscsc@icloud.com>
2023-12-13 11:58:44 -0300

    Req 03: cria ProjectView

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from projects.models import Profile
 from projects.serializers.profile_serializer import ProfileSerializer
+from projects.serializers.project_serializer import ProjectSerializer
 
 
 # Create your views here.
 class ProfileView(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProjectSerializer
